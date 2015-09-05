0b6e9fe015e74b81cfe2af80223aa869774ad867
Danny Paz <dan.pazuchowski@gmail.com>
2015-09-05 00:01:45 -0500

    added minify and edits to ignore for build

--- GulpFile.js
+++ GulpFile.js
@@ -2,25 +2,28 @@ var gulp = require('gulp')
   , jshint = require('gulp-jshint')
   , uglify = require('gulp-uglify')
   , notify = require('gulp-notify')
-  , rename = require('gulp-rename');
+  , rename = require('gulp-rename')
+  , del = require('del');
 
-var files {
+var files = {
   js: [
-    './src/app.js',
     './src/directive/trufflecake.js'
   ]
 };
 
 gulp.task('minified')
 
-gulp.task('default', function(){
+gulp.task('default', ['clean'], function(){
   return gulp.src(files.js)
     // You can add options to jslint with {}
     .pipe(jshint())
     .pipe(gulp.dest('dist/'))
-    .pipe()
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('dist/'))
     .pipe(notify({message: 'build complete'}));
+});
+
+gulp.task('clean', function(cb){
+  del(['dist/'], cb);
 });
